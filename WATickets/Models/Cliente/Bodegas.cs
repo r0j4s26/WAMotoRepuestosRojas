91a3c78d61e2bfbce84eaa3d8131e90bc0fc4afc
Daniel delas Cinco Ordilias <daniconix98@gmail.com>
2026-06-02 15:55:55 -0600

    backend bodegas
    controlador y cliente de bodegas

---
+++ WATickets/Models/Cliente/Bodegas.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WATickets.Models.Cliente
+{
+    public class Bodegas
+    {
+        public int id { get; set; }
+
+
+
+        [StringLength(500)]
+        public string Nombre { get; set; }
+    }
+}
