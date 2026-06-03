e70b6341e01bdcd74d3d8624526046d968b64ea9
Estabanfide <137965181+Estabanfide@users.noreply.github.com>
2026-06-03 15:53:15 -0600

    Models Cliente DetOferta EncOferta Oferta Proveedo

---
+++ WATickets/Models/Cliente/EncOferta.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace WATickets.Models.Cliente
+{
+    [Table("EncOferta")]
+    public class EncOferta
+    {
+        public int id { get; set; }
+        public int idCliente { get; set; }
+        public int idCondPago { get; set; }
+        public int idUsuarioCreador { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentarios { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal TotalImpuestos { get; set; }
+        public decimal TotalDescuentos { get; set; }
+        public decimal TotalCompra { get; set; }
+        public string Moneda { get; set; }
+        public string Status { get; set; }
+        public string TipoDocumento { get; set; }
+        public decimal Redondeo { get; set; }
+    }
+}
---
+++ WATickets/Models/Cliente/Ofertas.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WATickets.Models.Cliente
+{
+    public class Ofertas
+    {
+        public int id { get; set; }
+        public int idCliente { get; set; }
+        public int idCondPago { get; set; }
+        public DateTime Fecha { get; set; }
+        public string Comentarios { get; set; }
+        public decimal Subtotal { get; set; }
+        public decimal TotalImpuestos { get; set; }
+        public decimal TotalDescuentos { get; set; }
+        public decimal TotalCompra { get; set; }
+        public string Moneda { get; set; }
+        public string Status { get; set; }
+        public string TipoDocumento { get; set; }
+        public decimal Redondeo { get; set; }
+        public List<DetOferta> Detalle { get; set; }
+    }
+}
